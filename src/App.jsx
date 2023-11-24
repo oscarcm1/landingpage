8984444c53c277cb8ee22c1691947ffc7982e207
oscar <oscar.delfos369.com>
2023-11-24 17:40:41 -0600

    Slider infinito

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,33 @@ import './App.css'
 function App() {
 
   return (
-    <>
+    <div className='container'>
 
-     
-    </>
+     <section className='box'>
+      <div className="card">1</div>
+      <div className="card">2</div>
+      <div className="card">3</div>
+      <div className="card">4</div>
+      <div className="card">5</div>
+      <div className="card">6</div>
+      <div className="card">7</div>
+      <div className="card">8</div>
+      <div className="card">9</div>
+      <div className="card">10</div>
+      
+      <div className="card">1</div>
+      <div className="card">2</div>
+      <div className="card">3</div>
+      <div className="card">4</div>
+      <div className="card">5</div>
+      <div className="card">6</div>
+      <div className="card">7</div>
+      <div className="card">8</div>
+      <div className="card">9</div>
+      <div className="card">10</div>
+    
+     </section>
+    </div>
   )
 }
 

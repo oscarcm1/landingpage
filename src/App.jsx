719b6203551933fa320bc7be8f31450a4f4ab95e
oscar <oscar2905@gmail.com>
2023-12-18 18:55:52 -0600

    template

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,19 @@ import About from './pages/About/About'
 import Services from './pages/Services/Services'
 import Footer from './pages/Footer/Footer'
 import Choose from './pages/Choose/Choose'
-import Videos from './pages/Videos/Videos'
 
 
 function App() {
 
   return (
     <div className='page'>
-
-      <Navbar/> 
+      <Navbar/>
       <About/>
       <Services/>
       <Choose/>
-      <Videos/>
       <Footer/>
-    
-
       <div className='bu'></div>
       <div className='bu2'></div>
-
-
     </div>
   )
 }
--- src/pages/navbar/Navbar.jsx
+++ src/pages/navbar/Navbar.jsx
@@ -7,11 +7,10 @@ function Navbar() {
         <nav className='menu'>
             <h1>Logo</h1>
             <ul className='list'>
-                <li><a href="">Inicio</a></li>
-                <li><a href="">Sobre mí</a></li>
-                <li><a href="">Servicios</a></li>
-                <li><a href="">Contacto</a></li>
-                <li><a href="" className='color'>Get Started</a></li>
+                <li><a href="">Destinations</a></li>
+                <li><a href="">Booking</a></li>
+                <li><a href="">Gallery</a></li>
+                <li><a href="">Find a tour</a></li>
             </ul>
         </nav>
 

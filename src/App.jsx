f31d65f4cd171970433a4312d629ff4e645351f9
oscar <oscar2905@gmail.com>
2024-01-05 05:47:46 -0600

    template master

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,11 @@
 
 import './App.css'
-import Navbar from './pages/navbar/Navbar'
-import About from './pages/About/About'
-import Services from './pages/Services/Services'
-import Footer from './pages/Footer/Footer'
-import Choose from './pages/Choose/Choose'
 
 function App() {
 
   return (
-    <div className='page'>
-
-      <Navbar/> 
-      <About/>
-      <Services/>
-      <Choose/>
-      <Footer/>
-    
-
-      <div className='bu'></div>
-      <div className='bu2'></div>
-
-
+    <div className='container'>
+      
     </div>
   )
 }

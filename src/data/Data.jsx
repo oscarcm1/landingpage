873d9470077dc472223d4513b82a35656668ecdb
oscar <oscar2905@gmail.com>
2024-01-12 07:39:27 -0600

    answers and questions v1

---
+++ src/data/Data.jsx
@@ -0,0 +1,116 @@
+export const Data = [
+    {
+      "question": "¿Cuál es la capital de Francia?",
+      "answer":"A",
+      "answers": [
+        {
+          "A": "París",
+          "B": "Londres",
+          "C": "Madrid",
+          "D": "Berlín"
+        }
+      ]
+    },
+    {
+      "question": "¿Quién escribió 'Cien años de soledad'?",
+      "answer":"A",
+      "answers": [
+        {
+          "A": "Gabriel García Márquez",
+          "B": "Julio Cortázar",
+          "C": "Mario Vargas Llosa",
+          "D": "Pablo Neruda"
+        }
+      ]
+    },
+    {
+      "question": "¿Cuál es el río más largo del mundo?",
+      "answer":"A",
+      "answers": [
+        {
+          "A": "Nilo",
+          "B": "Amazonas",
+          "C": "Yangtsé",
+          "D": "Misisipi"
+        }
+      ]
+    },
+    
+
+    {
+        "question": "¿Cuál es la capital de Francia?",
+        "answer":"A",
+        "answers": [
+          {
+            "A": "París",
+            "B": "Londres",
+            "C": "Madrid",
+            "D": "Berlín"
+          }
+        ]
+      },
+      {
+        "question": "¿Quién escribió 'Cien años de soledad'?",
+        "answer":"A",
+        "answers": [
+          {
+            "A": "Gabriel García Márquez",
+            "B": "Julio Cortázar",
+            "C": "Mario Vargas Llosa",
+            "D": "Pablo Neruda"
+          }
+        ]
+      },
+      {
+        "question": "¿Cuál es el río más largo del mundo?",
+        "answer":"A",
+        "answers": [
+          {
+            "A": "Nilo",
+            "B": "Amazonas",
+            "C": "Yangtsé",
+            "D": "Misisipi"
+          }
+        ]
+      },
+      {
+        "question": "¿Cuál es la capital de Francia?",
+        "answer":"A",
+        "answers": [
+          {
+            "A": "París",
+            "B": "Londres",
+            "C": "Madrid",
+            "D": "Berlín"
+          }
+        ]
+      },
+      {
+        "question": "¿Quién escribió 'Cien años de soledad'?",
+        "answer":"A",
+        "answers": [
+          {
+            "A": "Gabriel García Márquez",
+            "B": "Julio Cortázar",
+            "C": "Mario Vargas Llosa",
+            "D": "Pablo Neruda"
+          }
+        ]
+      },
+      {
+        "question": "¿Cuál es el río más largo del mundo?",
+        "answer":"A",
+        "answers": [
+          {
+            "A": "Nilo",
+            "B": "Amazonas",
+            "C": "Yangtsé",
+            "D": "Misisipi"
+          }
+        ]
+      },
+    
+    
+    
+  ]
+  

c9cabf7d5e416d7d2cd7888f0b1bed5aa95af1dd
oscar <oscar2905@gmail.com>
2024-02-13 16:30:05 -0600

    Landing Product

--- src/Page.jsx
+++ src/Page.jsx
@@ -6,138 +6,120 @@ function Page() {
 
             <header className='menu'>
                 <h1>Logo</h1>
-                <nav>
-                    <ul>
-                        <li><a href="">About</a></li>
-                        <li><a href="">Blog</a></li>
-                        <li><a href="">Services</a></li>
-                        <li><a className='color' href="">Contact Us</a></li>
-                    </ul>
-                </nav>
+                <a href="" className='color'>Sign in</a>
             </header>
 
             <section className='slider'>
                 <article>
-                    <h2>Our Story</h2>
-                    <p>Lorem ipsum dolor sit amet. Id quos vero qui quisquam sapiente et totam quia id galisum animi eum voluptatum neque. Est facilis minima id quod soluta eos reiciendis sapiente et tempore exercitationem. Qui dolorum rerum nam eius quam aut reprehenderit culpa. Aut dolores amet ut nihil dolor et vitae laborum.</p>
+                    <h2>Summer Season</h2>
+                    <h3>Healthy Diet</h3>
+                    <h4>Strawberry fields</h4>
+                    <p>We are having huge collection of Healthy Juices foreveryone.</p>
+                    <a href="" className='color'>Explore</a>
                 </article>
-
-                <article>
-                    <img src="https://img.freepik.com/vector-gratis/ilustracion-concepto-expertos_114360-451.jpg?w=740&t=st=1707855405~exp=1707856005~hmac=a293015ec05374279a7c4bc6051fa4635ee66848245684824d40b20de0623bb0" alt="" />
-                </article>
-            </section>
-
-
-
-            <section className='scores'>
-                <article>
-                    <h3>12</h3>
-                    <p>Years of Industrie</p>
-                </article>
-
-                <article>
-                    <h3>75</h3>
-                    <p>Destinations</p>
-                </article>
-                <article>
-                    <h3>12</h3>
-                    <p>Years of Expirence</p>
-                </article>
-
-                <article>
-                    <h3>250</h3>
-                    <p>Clients</p>
+                <article className='product'>
+                    <img src="https://img.freepik.com/foto-gratis/batido-fresa-platano-fresco-tarro-aislado-sobre-fondo-blanco_123827-20726.jpg?w=360&t=st=1707858790~exp=1707859390~hmac=bd651134244289352c25e0a888b5436fd5dcb9c904e8e2fea1321254d3885360" alt="" />
                 </article>
             </section>
 
 
-            <section className='travel'>
-
-                <h2>Let´s Travel</h2>
-
+            <section className='box-product'>
                 <article>
-                    <img src="https://img.freepik.com/vector-gratis/trofeo_78370-345.jpg?w=740&t=st=1707856014~exp=1707856614~hmac=70d9489bf6ccc8488d61c6ad42dfbc3a412b83e34deb54d7f0654c1820c908c5" alt="" />
-                    <div className='info'>
-                        <h4>We have the expirence under our belt</h4>
-                        <p>Aut voluptas nulla est assumenda nihil a blanditiis voluptatem? Et impedit doloremque et facilis delectus et quia cumque. Nam consectetur consequatur aut alias repellat eum deleniti accusamus ut eaque magnam et illum dolores non reiciendis fugit est dicta voluptatibus.</p>
-                    </div>
+                    <img src="https://img.freepik.com/vector-premium/fondo-fresco-bebidas-helados-verano_1013976-471.jpg?w=740" alt="" />
+                    <h3>Chocolate Ice cream</h3>
+                    <p>Best construction company sloga your outer world structi.</p>
                 </article>
-
                 <article>
-                    <div className='info'>
-                        <h4>Collaborations</h4>
-                        <p>Aut voluptas nulla est assumenda nihil a blanditiis voluptatem? Et impedit doloremque et facilis delectus et quia cumque. Nam consectetur consequatur aut alias repellat eum deleniti accusamus ut eaque magnam et illum dolores non reiciendis fugit est dicta voluptatibus.</p>
-                    </div>
-                    <img src="https://img.freepik.com/vector-premium/apreton-manos-empresarial-informes-presentaciones_156780-50.jpg?w=740" alt="" />
+                    <img src="https://img.freepik.com/vector-premium/fondo-fresco-bebidas-helados-verano_1013976-471.jpg?w=740" alt="" />
+                    <h3>Chocolate Ice cream</h3>
+                    <p>Best construction company sloga your outer world structi.</p>
                 </article>
-
                 <article>
-                    <img src="https://img.freepik.com/vector-gratis/ilustracion-concepto-alcancia_114360-5612.jpg?w=740&t=st=1707856484~exp=1707857084~hmac=0967aefa7323d3eeec3cf97fd814f3f3137b93ddfc0e384ee5ae9608a9521986" alt="" />
-                    <div className='info'>
-                        <h4>Affordable Pricing</h4>
-                        <p>Aut voluptas nulla est assumenda nihil a blanditiis voluptatem? Et impedit doloremque et facilis delectus et quia cumque. Nam consectetur consequatur aut alias repellat eum deleniti accusamus ut eaque magnam et illum dolores non reiciendis fugit est dicta voluptatibus.</p>
-                    </div>
+                    <img src="https://img.freepik.com/vector-premium/fondo-fresco-bebidas-helados-verano_1013976-471.jpg?w=740" alt="" />
+                    <h3>Chocolate Ice cream</h3>
+                    <p>Best construction company sloga your outer world structi.</p>
                 </article>
             </section>
 
 
-            <section className='team'>
+            <section className='cakes'>
+                <h2>Cakes</h2>
 
-                <h3>Our Team</h3>
-
-
-                <div className='team-pictures'>
-                    <article className='profile'>
-                        <img src="https://img.freepik.com/foto-gratis/retrato-nina-joven-sonriente_23-2149260597.jpg?w=740&t=st=1707856669~exp=1707857269~hmac=c5e642b57d0fb6e402055035fc59da6e2c53a18421434224785031f01c898905" alt="" />
-                        <h4>Sheldon</h4>
-                        <p>Co Founder</p>
-                    </article>
-                    <article className='profile'>
-                        <img src="https://img.freepik.com/foto-gratis/retrato-nina-joven-sonriente_23-2149260597.jpg?w=740&t=st=1707856669~exp=1707857269~hmac=c5e642b57d0fb6e402055035fc59da6e2c53a18421434224785031f01c898905" alt="" />
-                        <h4>Sheldon</h4>
-                        <p>Co Founder</p>
-                    </article>
-                    <article className='profile'>
-                        <img src="https://img.freepik.com/foto-gratis/retrato-nina-joven-sonriente_23-2149260597.jpg?w=740&t=st=1707856669~exp=1707857269~hmac=c5e642b57d0fb6e402055035fc59da6e2c53a18421434224785031f01c898905" alt="" />
-                        <h4>Sheldon</h4>
-                        <p>Co Founder</p>
+                <div className='list-cakes'>
+                    <article>
+                        <img src="https://img.freepik.com/fotos-premium/magdalena-glaseado-rosa-chispas_391229-9055.jpg?w=740" alt="" />
+                        <h3>Chocolate cake</h3>
+                        <p>Description</p>
+                        <button>Buy</button>
                     </article>
 
+                    <article>
+                        <img src="https://img.freepik.com/fotos-premium/magdalena-glaseado-rosa-chispas_391229-9055.jpg?w=740" alt="" />
+                        <h3>Chocolate cake</h3>
+                        <p>Description</p>
+                        <button>Buy</button>
+                    </article>
+
+                    <article>
+                        <img src="https://img.freepik.com/fotos-premium/magdalena-glaseado-rosa-chispas_391229-9055.jpg?w=740" alt="" />
+                        <h3>Chocolate cake</h3>
+                        <p>Description</p>
+                        <button>Buy</button>
+                    </article>
+                </div>
+            </section>
+
+
+            <section className='coffee'>
+                <h2>Coffee Menu</h2>
+                <div className='box-coffee'>
+                    <article>
+                        <img src="https://img.freepik.com/foto-gratis/splash-cafe-granos-cafe-sobre-fondo-blanco_125540-3366.jpg?w=740&t=st=1707861165~exp=1707861765~hmac=8e525253f709df2771dab6bac78c7e2afa1952abd323ff9358154bb8ccf0d99e" alt="" />
+                    </article>
+                    <article className='info'>
+                        <h3>We welcomes you!</h3>
+                        <h4>Come of the best cup of coffee</h4>
+                        <p>Vel commodi vitae 33 ullam voluptatibus ut ipsa iure. Ut dolores nihil 33 repudiandae possimus aut aperiam distinctio et ipsum recusandae et dicta veritatis sit suscipit quae. Aut mollitia iusto qui natus excepturi qui libero exercitationem in Quis quae non nisi earum. Qui ipsum iure et nostrum voluptatem ut excepturi quas ut ipsa itaque et optio laudantium.</p>
+                        <div className='btn'>
+                            <button>Read more</button>
+                            <button>Buy</button>
+                        </div>
+                    </article>
                 </div>
             </section>
 
 
 
+          <footer className='footer'>
+
+            <ul>
+                <h4>SITEMAP</h4>
+                <li><a href="">Home</a></li>
+                <li><a href="">Product</a></li>
+                <li><a href="">Pricing</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+
+            <ul>
+                <h4>SOCIAL MEDIA</h4>
+                <li><a href="">Linkedin</a></li>
+                <li><a href="">Instagram</a></li>
+                <li><a href="">Facebook</a></li>
+                <li><a href="">TikTok</a></li>
+            </ul>
+
+            <ul>
+                <h4>HELP</h4>
+                <li><a href="">Getting started</a></li>
+                <li><a href="">Referral program</a></li>
+                <li><a href="">FAQ</a></li>
+            </ul>
+            
 
 
 
-            <section className='contact'>
 
-                <h3>Get in touch with us</h3>
-
-
-                <div className='footer'>
-                    <article>
-                        <p>Email</p>
-                        <a href="">hello@gmail.com</a>
-                    </article>
-                    <article>
-                        <p>Location</p>
-                        <a href="">Address</a>
-                    </article>
-                    <article>
-                        <p>Phone</p>
-                        <a href="">+52</a>
-                    </article>
-
-                </div>
-
-
-            </section>
-
-
-
-
+          </footer>
 
 
 

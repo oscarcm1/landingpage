de42bff812d9b68adb9c40cfa67739814b0851c5
oscar <oscar2905@gmail.com>
2024-02-21 14:50:47 -0600

    slider con map

--- src/Page.jsx
+++ src/Page.jsx
@@ -1,5 +1,40 @@
 import React from 'react'
 
+
+export let slides =[
+    {
+        "id": 1,
+        "title": "Summer Season",
+        "subtitle": "Healthy Diet",
+        "product": "Strawberry fields",
+        "description": "We are having huge colection of Healthy juices for everyone.",
+        "button": "Buy now",
+        "img": "https://img.freepik.com/foto-gratis/batido-fresa-platano-fresco-tarro-aislado-sobre-fondo-blanco_123827-20726.jpg?w=360&t=st=1707858790~exp=1707859390~hmac=bd651134244289352c25e0a888b5436fd5dcb9c904e8e2fea1321254d3885360",
+        "alt": "Strawberry"
+    },
+    {
+        "id": 2,
+        "title": "Winter Season",
+        "subtitle": "Healthy Diet",
+        "product": "Chocolate fields",
+        "description": "We are having huge colection of Healthy juices for everyone.",
+        "button": "Buy now",
+        "img": "https://img.freepik.com/fotos-premium/leche-chocolate-o-bebida-cacao-vertiendo-vaso_1004890-4364.jpg?w=360",
+        "alt": "Strawberry"
+    },
+    {
+        "id": 3,
+        "title": "Spring Season",
+        "subtitle": "Healthy Diet",
+        "product": "Chocolate fields",
+        "description": "We are having huge colection of Healthy juices for everyone.",
+        "button": "Buy now",
+        "img": "https://img.freepik.com/foto-gratis/delicioso-batido-platano_144627-27496.jpg?w=360&t=st=1708376419~exp=1708377019~hmac=e2149493a3fca734fef6082163a4175579a9e6c3bdd48594be497d2f5d832005",
+        "alt": "Strawberry"
+    },
+];
+
+
 function Page() {
 
     let currentIndex = 0;
@@ -21,55 +56,34 @@ function Page() {
         slider.style.transform = `translateX(-${currentIndex * 100}%)`;
     }
 
+     setInterval(nextSlide, 3000);
+
 
 
     return (
         <div className='container'>
             <div className="slider-container">
                 <div className="slider">
-                    <section className="slide">
+                    {slides.map((key)  =>
+                    <section className="slide" key={key.id}>
                         <article>
-                            <h2>Summer Season</h2>
-                            <h3>Healthy Diet</h3>
-                            <h4>Strawberry fields</h4>
-                            <p>We are having huge colection of Healthy juices for everyone.</p>
-                            <button>Buy now</button>
+                            <h2>{key.title}</h2>
+                            <h3>{key.subtitle}</h3>
+                            <h4>{key.product}</h4>
+                            <p>{key.description}</p>
+                            <button>{key.button}</button>
                         </article>
                         <article>
-                            <img src="https://img.freepik.com/foto-gratis/batido-fresa-platano-fresco-tarro-aislado-sobre-fondo-blanco_123827-20726.jpg?w=360&t=st=1707858790~exp=1707859390~hmac=bd651134244289352c25e0a888b5436fd5dcb9c904e8e2fea1321254d3885360" alt="" />
+                            <img src={key.img} alt={key.alt} />
                         </article>
-                    </section>
-                    <section className="slide">
-                        <article>
-                            <h2>Winter Season</h2>
-                            <h3>Healthy Diet</h3>
-                            <h4>Chocolate fields</h4>
-                            <p>Description ...</p>
-                            <button>Buy</button>
-                        </article>
-                        <article>
-                            <img src="https://img.freepik.com/fotos-premium/leche-chocolate-o-bebida-cacao-vertiendo-vaso_1004890-4364.jpg?w=360" alt="" />
-                        </article>
-                    </section>
-                    <section className="slide">
-                        <article>
-                            <h2>Spring Season</h2>
-                            <h3>Healthy Diet</h3>
-                            <h4>Strawberry fields</h4>
-                            <p>Description ...</p>
-                            <button>Buy</button>
-                        </article>
-                        <article>
-                            <img src="https://img.freepik.com/foto-gratis/delicioso-batido-platano_144627-27496.jpg?w=360&t=st=1708376419~exp=1708377019~hmac=e2149493a3fca734fef6082163a4175579a9e6c3bdd48594be497d2f5d832005" alt="" />
-                        </article>
-                    </section>
+                    </section>              
+                    ) }
                 </div>
                 <section className="controls">
                     <button onClick={prevSlide}><p>{"<"}</p></button>
                     <button onClick={nextSlide}><p>{">"}</p></button>
                 </section>
             </div>
-
         </div>
     )
 }
